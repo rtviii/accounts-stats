de348da6bf57b3257ea89ff662f60d5ae4e4abf5
Artem <rtkushner@gmail.com>
2022-07-29 22:37:19 -0700

    crossbeam works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crossbeam::thread::Scope;
 use crossbeam_utils::thread;
 use std::{
     fmt::format,
@@ -21,22 +22,42 @@ pub fn threcho(msg: &str) {
 // pub fn spawn_worker_for_chunk(data: &[u64], send_to_master: Sender<u64>, chunksize: u64) {
 // }
 
+
+pub fn spawn_worker_in_scope<'a>(s:&Scope<'a>, data: &'a[u64], send_to_master: Sender<u64>, chunksize: u64) {
+    s.spawn(move|_| {
+        println!(" {:?} <-", std::thread::current().id());
+            println!("A child thread borrowing `var`: {:?}", &data[0]);
+            send_to_master.send(data[0]).expect("msdasdag");
+    });
+}
+
 fn main() {
     // MASTER THREAD
     let data = (1..10_000).collect::<Vec<u64>>();
     let (sd, rx): (Sender<u64>, Receiver<u64>) = mpsc::channel();
     let sender = sd.clone();
 
-    let var = vec![1, 2, 3];
+
 
     thread::scope(|s| {
-        let jh1 = s.spawn(|_| {
-            println!("Going to sleep for 3 sec.");
-            std::thread::sleep(Duration::from_secs(3));
-        });
-        let jh2 =s.spawn(|_| {
-            println!("A child thread borrowing `var`: {:?}", &var);
-        });
+
+
+        spawn_worker_in_scope(s, &data[0..10], sd.clone(), 10);
+        spawn_worker_in_scope(s, &data[10..20], sd.clone(), 10);
+        spawn_worker_in_scope(s, &data[20..30], sd.clone(), 10);
+
+
+        // let jh1 = s.spawn(|_| {
+        //     println!("Going to sleep for 3 sec.");
+        //     std::thread::sleep(Duration::from_secs(3));
+        //     println!("woke up");
+        // });
+        // // jh1.join();
+
+        // let jh2 = s.spawn(|_| {
+        //     println!("A child thread borrowing `var`: {:?}", &data[0]);
+        // });
+
     })
     .unwrap();
 
