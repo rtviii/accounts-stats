4843db7feda989e02280ae14afd698d1ed063ea6
Artem <rtkushner@gmail.com>
2022-07-29 21:58:13 -0700

    receiving on main

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,41 @@
-use std::{thread, sync::mpsc::{self, Sender, Receiver}};
+use std::{thread, sync::mpsc::{self, Sender, Receiver}, fmt::format};
 
 
 pub fn threcho(msg:&str){
     println!("[{:?}]: {}",thread::current().id(), msg);
 }
 
+
+
 fn main() {
 
-    let me = std::thread::current().id();
-    
-
+    // MASTER THREAD
+    let data = (1..10_000).collect::<Vec<u64>>();
     let (sd, rx):( Sender<u64>, Receiver<u64> ) = mpsc::channel();
-
-
-    let y = sd.clone();
-    let x = thread::spawn(move|| {
-
-        let me = std::thread::current().id();
-        // println!("[ {:?} ] sending 1.", me);
-        threcho("sending 1");
-        y.send(1);
-        println!("Hello from a thread!");
-
+    let sender = sd.clone();
+    let worker = thread::spawn(move|| {
+        // WORKER thread
+        // process a batch of 10000, return.
+    
+        for chunk in data.chunks(100){
+            let mut sum = 0;
+            for i in chunk.iter(){
+                sum +=i;
+            }
+            threcho(&format!("sending sum({}) to master", sum));
+            sender.send(sum).expect("Could not send.");
+        }
     });
 
 
-
-    x.join();
-
-
+    // worker.join();
 
     
-    let got = rx.recv().unwrap();
-    println!();
+    while true {
+        let got = rx.recv().unwrap();
+        threcho(&format!("Got {}", got));
+    };
 
-    
-    threcho("received 1");
 
     
 }
