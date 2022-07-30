c1cd93c7c4d815e5318fc8501ee04dc0ebb3e699
Artem <rtkushner@gmail.com>
2022-07-29 22:28:29 -0700

    experimenting with scoped thread

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,63 @@
-use std::{thread, sync::mpsc::{self, Sender, Receiver}, fmt::format};
+use crossbeam_utils::thread;
+use std::{
+    fmt::format,
+    sync::mpsc::{self, Receiver, Sender},
+    time::Duration,
+};
 
-
-pub fn threcho(msg:&str){
-    println!("[{:?}]: {}",thread::current().id(), msg);
+pub fn threcho(msg: &str) {
+    println!("[{:?}]: {}", std::thread::current().id(), msg);
 }
 
-
+// WORKER thread
+// process a batch of 100, return.
+// for chunk in data.into_iter(){
+//     let mut sum = 0;
+//     // for i in chunk.iter(){
+//     //     sum +=i;
+//     //
+//     threcho(&format!("sending sum({}) to master", sum));
+//     send_to_master.send(sum).expect("Could not send.");
+// pub fn spawn_worker_for_chunk(data: &[u64], send_to_master: Sender<u64>, chunksize: u64) {
+// }
 
 fn main() {
-
     // MASTER THREAD
     let data = (1..10_000).collect::<Vec<u64>>();
-    let (sd, rx):( Sender<u64>, Receiver<u64> ) = mpsc::channel();
+    let (sd, rx): (Sender<u64>, Receiver<u64>) = mpsc::channel();
     let sender = sd.clone();
-    let worker = thread::spawn(move|| {
-        // WORKER thread
-        // process a batch of 10000, return.
-    
-        for chunk in data.chunks(100){
-            let mut sum = 0;
-            for i in chunk.iter(){
-                sum +=i;
-            }
-            threcho(&format!("sending sum({}) to master", sum));
-            sender.send(sum).expect("Could not send.");
-        }
-    });
 
+    let var = vec![1, 2, 3];
+
+    thread::scope(|s| {
+        let jh1 = s.spawn(|_| {
+            println!("Going to sleep for 3 sec.");
+            std::thread::sleep(Duration::from_secs(3));
+        });
+        let jh2 =s.spawn(|_| {
+            println!("A child thread borrowing `var`: {:?}", &var);
+        });
+    })
+    .unwrap();
+
+    // let worker = thread::spawn(move|| {
+    //     // WORKER thread
+    //     // process a batch of 10000, return.
+
+    //     for chunk in data.chunks(100){
+    //         let mut sum = 0;
+    //         for i in chunk.iter(){
+    //             sum +=i;
+    //         }
+    //         threcho(&format!("sending sum({}) to master", sum));
+    //         sender.send(sum).expect("Could not send.");
+    //     }
+    // });
 
     // worker.join();
 
-    
     while true {
         let got = rx.recv().unwrap();
         threcho(&format!("Got {}", got));
-    };
-
-
-    
+    }
 }
