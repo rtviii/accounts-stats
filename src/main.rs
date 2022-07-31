04a751cc888412139b7e8b5dca14113dbfa79609
Artem <rtkushner@gmail.com>
2022-07-30 19:20:53 -0700

    got crossbeam working with base consumer. woot woot

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use crossbeam::thread::Scope;
-use crossbeam_utils::thread;
+use clap::Parser;
+use crossbeam::thread::{self, Scope};
 use log::{info, warn};
+use rdkafka::client::ClientContext;
+use rdkafka::config::ClientConfig;
+use rdkafka::consumer::base_consumer::{self, BaseConsumer};
+use rdkafka::consumer::Consumer;
+use rdkafka::error::KafkaError;
+use rdkafka::producer::ThreadedProducer;
+use rdkafka::producer::{DeliveryResult, ProducerContext};
+use rdkafka::Message;
+use rdkafka::TopicPartitionList;
 use std::{
-    fmt::format,
     sync::mpsc::{self, Receiver, Sender},
     time::Duration,
 };
-use futures::{Stream, StreamExt};
-use rdkafka::{producer::{ BaseProducer, BaseRecord , DeliveryResult, ProducerContext}, statistics::Topic, consumer::{StreamConsumer, MessageStream}};
-use rand::RngCore;
-use rdkafka::consumer::base_consumer::{self, BaseConsumer};
-use rdkafka::{config::FromClientConfig, TopicPartitionList};
-use serde_json::Value;
-use std::sync::atomic::{AtomicIsize, Ordering};
-use std::thread::JoinHandle;
-use rdkafka::client::ClientContext;
-use rdkafka::config::ClientConfig;
-use rdkafka::consumer::{Consumer, DefaultConsumerContext};
-use rdkafka::producer::ThreadedProducer;
-use rdkafka::Message;
-use clap::Parser;
 
 struct ProducerLogger {}
 impl ClientContext for ProducerLogger {}
@@ -58,173 +52,135 @@ pub fn producer() {
         .set("bootstrap.servers", "localhost:9095")
         .create_with_context(ProducerLogger {})
         .expect("Failed to create producer");
-
 }
 
-pub fn consumer(){
+pub fn consumer() {
     let mut baseconsumer: BaseConsumer = ClientConfig::new()
         .set("bootstrap.servers", "127.0.0.1:9095")
         // .set("enable.auto.commit", "false")
         .set("group.id", "rt0")
         .create()
         .expect("Failed to create consumer");
-
 }
-
-
 
 #[derive(Debug, Parser)]
 #[clap(author, version, long_about=None, about = "Prototype of a tool to extract data from a set of blocks + threading experiments")]
 pub struct Args {
-
-    #[clap(takes_value = false,  long)]
+    #[clap(takes_value = false, long)]
     producer: bool,
 
-    #[clap(takes_value = false,  long)]
+    #[clap(takes_value = false, long)]
     consumer: bool,
 
-    #[clap(short='g', long)]
+    #[clap(short = 'g', long)]
     consumer_group: Option<String>,
 
-    #[clap(short='p', long)]
-    broker_port: usize
+    #[clap(short = 'p', long)]
+    broker_port: usize,
+}
+
+const TOPIC: &'static str = "historic_blocks_json";
+pub async fn process_region(
+    port          : usize,
+    consumer_group: &str,
+    offset_start  : usize,
+    offset_end    : usize,
+    topic         : &str,
+    chl_sender    : Sender<i64>,
+) {
 }
 
 #[tokio::main]
 async fn main() {
-
-    let args      = Args::parse();
-
+    let args = Args::parse();
     let flag_cons = args.consumer;
     let flag_prod = args.producer;
+    let port = &args.broker_port;
+    let consumer_group = &args
+        .consumer_group
+        .unwrap_or("default_consumer_group".to_string());
 
-    let port           = &args.broker_port;
-    let consumer_group = &args.consumer_group.unwrap_or("default_consumer_group".to_string());
-
-    let mut baseconsumer: BaseConsumer = ClientConfig::new()
-        .set("bootstrap.servers", format!("127.0.0.1:{}",port))
-        // .set("enable.auto.commit", "false")
-        .set("group.id", consumer_group)
-        .create()
-        .expect("Failed to create consumer");
-
-
-        let mut stream :StreamConsumer = ClientConfig::new()
-        .set("bootstrap.servers", format!("127.0.0.1:{}",port))
-        .set("enable.partition.eof", "false")
-        .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "true")
-        // .set("enable.auto.commit", "false")
-        .set("group.id", consumer_group)
-        .create()
-        .expect("Failed to create consumer");
+    let (tx, rx) = mpsc::channel();
 
 
 
+    thread::scope(|s| {
+        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5400, 5300);
+        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5100, 5000);
+        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5200, 5100);
+        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5700, 5600);
+    })
+    .unwrap();
 
-    
-    let mut tpl = TopicPartitionList::new();
-    const TOPIC :&'static str  =  "historic_blocks_json";
-    tpl.add_partition_offset(TOPIC, 0, rdkafka::Offset::Beginning);
-    println!("{:?}", tpl);
-    stream.assign(&tpl).unwrap();
-
-    stream.seek(TOPIC, 0, rdkafka::Offset::Offset(16000), Duration::from_secs(2)).unwrap();
-
-    let mut counter  = 0;
-    loop{
-        println!("Counter :{ }", counter);
-        match stream.stream().next().await{
-                None => warn!("Kafka none"),
-           Some(m)   => {
-            counter +=1;
-            println!("m: {:?}", m.unwrap().payload())
-           }
-                // if let Some(headers) = m.headers() {
-                //     for header in headers.iter() {
-                //         info!("  Header {:#?}: {:?}", header.key, header.value);
-                //     }
-                // }
-                // consumer.commit_message(&m, CommitMode::Async).unwrap();
-
+    loop {
+        match rx.recv(){
+            Ok(msg) => {
+                threcho(&format!("Got {}", msg));
+            }
+            Err(e) => {
+                println!("{}", e);
+                std::thread::sleep(Duration::from_secs(2));
+            }
+            
+        }
     }
 
-    }
-    // while let msg =  stream.stream(){
-    //     let msg      = msgres.unwrap();
-    //     let key:&str = msg.key_view().unwrap().unwrap();
-    //     let value    = msg.payload().unwrap();
-    //     println!("Received message {} with value {:?}. Offset :{} | Partition :{}", key, value, msg.offset(),msg.partition());
-
-    // }
-    // baseconsumer.assign(&tpl).unwrap();
-    // // baseconsumer.seek("historic_blocks_json", 0, rdkafka::Offset::Offset(100), Duration::from_secs(3)).unwrap();
-    // for msgres in baseconsumer.iter(){
-    //     let msg      = msgres.unwrap();
-    //     let key:&str = msg.key_view().unwrap().unwrap();
-    //     let value    = msg.payload().unwrap();
-    //     println!("Received message {} with value {:?}. Offset :{} | Partition :{}", key, value, msg.offset(),msg.partition());
-    // }
-    
-    
-
-    // // MASTER THREAD
-    // let data = (1..10_000).collect::<Vec<u64>>();
-    // let (sd, rx): (Sender<u64>, Receiver<u64>) = mpsc::channel();
-    // let sender = sd.clone();
-    
-    // thread::scope(|s| {
-    //     spawn_worker_in_scope(s, &data[0..10], sd.clone(), 10);
-    //     spawn_worker_in_scope(s, &data[10..20], sd.clone(), 10);
-    //     spawn_worker_in_scope(s, &data[20..30], sd.clone(), 10);
-    // })
-    // .unwrap();
-
-    // loop {
-    //     let got = rx.recv().unwrap();
-    //     threcho(&format!("Got {}", got));
-    // }
-
-    // for i in 1..10_000_000 {
-    //     let mut data = [0u8; 8];
-    //     rand::thread_rng().fill_bytes(&mut data);
-    //     println!("Sedndg msg with data {:x?}", data);
-    //     producer
-    //         .send(
-    //             BaseRecord::to("rt-test")
-    //                 .key(&format!("key-{}", i))
-    //                 .payload(&format!("payload-{}", i)),
-    //         )
-    //         .expect("couldn't send message");
-    // }
-
-    // baseconsumer.subscribe(&[&"rt-test"]).expect("Couldnbt subscribe t topic.");
-    // return thread::spawn(move || loop{
-    // });
-
-    if flag_cons {
-        println!("Starting consumer");
-        let x = consumer();
-    } else if flag_prod {
-        producer();
-    }
-
-
-    // tokio_ex::main()
 }
-
-
 
 pub fn threcho(msg: &str) {
     println!("[{:?}]: {}", std::thread::current().id(), msg);
 }
 
-pub fn spawn_worker_in_scope<'a>(s:&Scope<'a>, data: &'a[u64], send_to_master: Sender<u64>, chunksize: u64) {
-    s.spawn(move|_| {
-        println!(" {:?} <-", std::thread::current().id());
-            println!("A child thread borrowing `var`: {:?}", &data[0]);
-            send_to_master.send(data[0]).expect("msdasdag");
+pub fn spawn_consumer_in_scope<'a>(
+    crossbeam_scope: &Scope<'a>,
+    send_to_master: Sender<i64>,
+    port: usize,
+    consumer_group: String,
+    offset_end: usize,
+    offset_start: usize,
+)->Result<(),KafkaError> {
+    crossbeam_scope.spawn(move |_| -> Result<(),KafkaError> {
+        let bconsumer = {
+            let mut bconsumer: BaseConsumer = ClientConfig::new()
+                .set("bootstrap.servers", format!("127.0.0.1:{}", port))
+                .set("enable.partition.eof", "false")
+                .set("session.timeout.ms", "6000")
+                .set("enable.auto.commit", "true")
+                // .set("enable.auto.commit", "false")
+                .set("group.id", consumer_group)
+                .create()
+                .expect("Failed to create consumer");
+
+            bconsumer
+        };
+
+        let mut tpl = TopicPartitionList::new();
+        tpl.add_partition_offset(TOPIC, 0, rdkafka::Offset::Offset(offset_start as i64))?;
+        println!("{:?}", tpl);
+        bconsumer.assign(&tpl)?;
+
+        
+        println!("Base consumer is at {:?}", bconsumer.assignment()?);
+        let mut range = offset_end - offset_start;
+        loop {
+            match bconsumer.poll(Duration::from_secs(2)) {
+                    Some(m) => {
+                        range -=1;
+                        let off= m?.offset();
+                        println!("Got message with offset :{:?}", off);
+                        send_to_master.send(off).unwrap();
+                    }
+                    None => {
+                        range -=1;
+                        println!("No message");
+                    }
+            }
+            if range < 1 {
+                println!("Stream completed.");
+                break;
+            };
+        }
+        Ok(())
     });
+    Ok(())
 }
-
-
