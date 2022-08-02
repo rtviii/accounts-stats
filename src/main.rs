dbbc1f7e134e5234e592cfc38cb86313a6e68d46
Artem <rtkushner@gmail.com>
2022-08-02 12:25:00 -0700

    moved parameters out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
 use clap::Parser;
 use crossbeam::thread::{self, Scope};
 use log::{info, warn};
@@ -10,61 +12,26 @@ use rdkafka::producer::ThreadedProducer;
 use rdkafka::producer::{DeliveryResult, ProducerContext};
 use rdkafka::Message;
 use rdkafka::TopicPartitionList;
+
 use std::{
-    sync::mpsc::{self, Receiver, Sender},
+    sync::mpsc::{self, Sender},
     time::Duration,
 };
 
-struct ProducerLogger {}
-impl ClientContext for ProducerLogger {}
-impl ProducerContext for ProducerLogger {
-    type DeliveryOpaque = ();
-    fn delivery(
-        &self,
-        delivery_result: &DeliveryResult<'_>,
-        delivery_opaque: Self::DeliveryOpaque,
-    ) {
-        let delivery_res = delivery_result.as_ref();
-        match delivery_res {
-            Ok(message) => {
-                let key: &str = message.key_view().unwrap().unwrap();
-                println!(
-                    "Produced message {} successfully. Offset: {}. Partition :{}",
-                    key,
-                    message.offset(),
-                    message.partition()
-                )
-                // This would probably be a good place to commit offsets
-            }
+pub fn parse_tuple(tup: &str) -> Result<(u64, u64), std::string::ParseError> {
+    let tup = tup.replace("(", "");
+    let tup = tup.replace(")", "");
+    let startend: Vec<_> = tup
+        .split(",")
+        .into_iter()
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
 
-            Err(producer_err) => {
-                let key: &str = producer_err.1.key_view().unwrap().unwrap();
-                println!(
-                    "Failed to produce message {}. Error: {}",
-                    key, producer_err.0
-                )
-            }
-        }
-    }
-}
-pub fn producer() {
-    let producer: ThreadedProducer<ProducerLogger> = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:9095")
-        .create_with_context(ProducerLogger {})
-        .expect("Failed to create producer");
-}
-
-pub fn consumer() {
-    let mut baseconsumer: BaseConsumer = ClientConfig::new()
-        .set("bootstrap.servers", "127.0.0.1:9095")
-        // .set("enable.auto.commit", "false")
-        .set("group.id", "rt0")
-        .create()
-        .expect("Failed to create consumer");
+    Ok((startend[0], startend[1]))
 }
 
 #[derive(Debug, Parser)]
-#[clap(author, version, long_about=None, about = "Prototype of a tool to extract data from a set of blocks + threading experiments")]
+#[clap(author, version, long_about=None)]
 pub struct Args {
     #[clap(takes_value = false, long)]
     producer: bool,
@@ -77,43 +44,79 @@ pub struct Args {
 
     #[clap(short = 'p', long)]
     broker_port: usize,
+
+    #[clap(short = 't', long)]
+    topic_name: String,
+
+    #[clap(long)]
+    n_threads: u64,
+
+    #[clap( long , short='s', value_parser=parse_tuple)]
+    start_end: (u64, u64),
 }
 
-const TOPIC: &'static str = "historic_blocks_json";
-pub async fn process_region(
-    port          : usize,
+pub fn baseconsumer_init(
+    port: usize,
     consumer_group: &str,
-    offset_start  : usize,
-    offset_end    : usize,
-    topic         : &str,
-    chl_sender    : Sender<i64>,
-) {
+    offset_range: &[u64],
+    topic: &str,
+) -> Result<BaseConsumer, KafkaError> {
+    let bconsumer = {
+        let c: BaseConsumer = ClientConfig::new()
+            .set("bootstrap.servers", format!("127.0.0.1:{}", port))
+            .set("enable.partition.eof", "false")
+            .set("session.timeout.ms", "6000")
+            .set("enable.auto.commit", "true")
+            .set("fetch.wait.max.ms", "100")
+            .set("batch.num.messages", "1")
+            // .set("enable.auto.commit", "false")
+            .set("group.id", consumer_group)
+            .create()
+            .expect("Failed to create consumer");
+        c
+    };
+    let mut tpl = TopicPartitionList::new();
+    tpl.add_partition_offset(topic, 0, rdkafka::Offset::Offset(offset_range[0] as i64))?;
+    bconsumer.assign(&tpl)?;
+    Ok(bconsumer)
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let args = Args::parse();
     let flag_cons = args.consumer;
     let flag_prod = args.producer;
     let port = &args.broker_port;
+    let topic_name = &args.topic_name;
+    let nthreads = args.n_threads;
+    // let workload       = &args.process_n_messages;
+    let startend = args.start_end;
+
+    // create n_threads equal chunks ranging from start_end[0] to start_end[1]
+    let offsets = (startend.0..startend.1).collect::<Vec<u64>>();
+    let chunks = offsets.chunks(offsets.len() / nthreads as usize);
+
     let consumer_group = &args
         .consumer_group
         .unwrap_or("default_consumer_group".to_string());
 
     let (tx, rx) = mpsc::channel();
 
-
-
-    thread::scope(|s| {
-        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5400, 5300);
-        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5100, 5000);
-        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5200, 5100);
-        spawn_consumer_in_scope(s, tx.clone(), 29092, "rt".to_string(), 5700, 5600);
+    let _ = thread::scope(|s| -> Result<(), KafkaError> {
+        for c in chunks {
+            let bconsumer = baseconsumer_init(*port, consumer_group, c, &topic_name)?;
+            let _ = spawn_consumer_in_scope(
+                bconsumer,
+                c[c.len()-1],
+                s,
+                tx.clone()
+            );
+        }
+        Ok(())
     })
     .unwrap();
 
     loop {
-        match rx.recv(){
+        match rx.recv() {
             Ok(msg) => {
                 threcho(&format!("Got {}", msg));
             }
@@ -121,10 +124,8 @@ async fn main() {
                 println!("{}", e);
                 std::thread::sleep(Duration::from_secs(2));
             }
-            
         }
     }
-
 }
 
 pub fn threcho(msg: &str) {
@@ -132,55 +133,75 @@ pub fn threcho(msg: &str) {
 }
 
 pub fn spawn_consumer_in_scope<'a>(
+    consumer: BaseConsumer,
+    halt_at_offset:u64,
     crossbeam_scope: &Scope<'a>,
     send_to_master: Sender<i64>,
-    port: usize,
-    consumer_group: String,
-    offset_end: usize,
-    offset_start: usize,
-)->Result<(),KafkaError> {
-    crossbeam_scope.spawn(move |_| -> Result<(),KafkaError> {
-        let bconsumer = {
-            let mut bconsumer: BaseConsumer = ClientConfig::new()
-                .set("bootstrap.servers", format!("127.0.0.1:{}", port))
-                .set("enable.partition.eof", "false")
-                .set("session.timeout.ms", "6000")
-                .set("enable.auto.commit", "true")
-                // .set("enable.auto.commit", "false")
-                .set("group.id", consumer_group)
-                .create()
-                .expect("Failed to create consumer");
-
-            bconsumer
-        };
-
-        let mut tpl = TopicPartitionList::new();
-        tpl.add_partition_offset(TOPIC, 0, rdkafka::Offset::Offset(offset_start as i64))?;
-        println!("{:?}", tpl);
-        bconsumer.assign(&tpl)?;
-
-        
-        println!("Base consumer is at {:?}", bconsumer.assignment()?);
-        let mut range = offset_end - offset_start;
+) -> Result<(), KafkaError> {
+    crossbeam_scope.spawn(move |_| -> Result<(), KafkaError> {
         loop {
-            match bconsumer.poll(Duration::from_secs(2)) {
-                    Some(m) => {
-                        range -=1;
-                        let off= m?.offset();
-                        println!("Got message with offset :{:?}", off);
-                        send_to_master.send(off).unwrap();
-                    }
-                    None => {
-                        range -=1;
-                        println!("No message");
-                    }
+            match consumer.poll(Duration::from_millis(1000)) {
+                Some(m) => {
+                    let message = m?;
+                    let off = message.offset();
+                    let sz = std::mem::size_of_val(message.payload().unwrap() );
+                    println!("Thread {:?} message with offset :{:?} size = {:?}", std::thread::current().id(),off,sz);
+
+                    send_to_master.send(off).unwrap();
+                }
+                None => {
+                    // println!("No message");
+                }
             }
-            if range < 1 {
-                println!("Stream completed.");
-                break;
-            };
         }
-        Ok(())
     });
     Ok(())
 }
+
+// struct ProducerLogger {}
+// impl ClientContext for ProducerLogger {}
+// impl ProducerContext for ProducerLogger {
+//     type DeliveryOpaque = ();
+//     fn delivery(
+//         &self,
+//         delivery_result: &DeliveryResult<'_>,
+//         delivery_opaque: Self::DeliveryOpaque,
+//     ) {
+//         let delivery_res = delivery_result.as_ref();
+//         match delivery_res {
+//             Ok(message) => {
+//                 let key: &str = message.key_view().unwrap().unwrap();
+//                 println!(
+//                     "Produced message {} successfully. Offset: {}. Partition :{}",
+//                     key,
+//                     message.offset(),
+//                     message.partition()
+//                 )
+//                 // This would probably be a good place to commit offsets
+//             }
+
+//             Err(producer_err) => {
+//                 let key: &str = producer_err.1.key_view().unwrap().unwrap();
+//                 println!(
+//                     "Failed to produce message {}. Error: {}",
+//                     key, producer_err.0
+//                 )
+//             }
+//         }
+//     }
+// }
+// pub fn producer() {
+//     let producer: ThreadedProducer<ProducerLogger> = ClientConfig::new()
+//         .set("bootstrap.servers", "localhost:9095")
+//         .create_with_context(ProducerLogger {})
+//         .expect("Failed to create producer");
+// }
+
+// pub fn consumer() {
+//     let mut baseconsumer: BaseConsumer = ClientConfig::new()
+//         .set("bootstrap.servers", "127.0.0.1:9095")
+//         // .set("enable.auto.commit", "false")
+//         .set("group.id", "rt0")
+//         .create()
+//         .expect("Failed to create consumer");
+// }
